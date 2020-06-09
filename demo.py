01a75f9112682f44068840f7811019026271f4a2
unknown <1586237561@qq.com>
2020-06-09 16:28:06 +0800

    python环境搭建和数据类型

--- demo.py
+++ demo.py
@@ -1,1 +1,56 @@
-print（"hello world"）
+'''
+print("hello world") #字符串
+print(2333)
+print(2.3333)
+print(True)  #布尔值
+print(False)
+print(())  #元组
+print([])  #数组
+print({})  #字典
+
+print("哈哈哈",2333,2.33333)
+print("还好"+"喜喜")
+print("喜喜"*100)
+
+print(1+2)
+print(((1+2)*100-99)/2)
+print(2<3)
+a = "张三"  #把张三这个值赋值给 变量a
+print(a)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a = float(input("请输入:"))
+# b = float(input("请输入:"))
+# print("input获取的内容:",a+b)
+
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.3333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+
+
+# a = str(2333)
+# print(type(a))
+
+a = input("努古you are:")
+b = input("金泰亨:")
+print("字数和",len(a)+len(b))
+
